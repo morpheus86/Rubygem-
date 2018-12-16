19d280941edbfa08db769a940f3acc6dd5e83e97
hamadou diallo <hamadou_diallo@yahoo.com>
2018-12-16 17:58:37 -0500

    .gitignore

---
+++ index.js
@@ -0,0 +1,37 @@
+const request = require("request");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+const RUBY_GEMS_URL = "https://rubygems.org";
+const PORT = 8000;
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+
+  next();
+});
+app.get("/hello", (req, res, next) => {
+  res.send("Hello");
+});
+app.get("/api/**/*", (req, res) => {
+  const url = `${RUBY_GEMS_URL}${req.originalUrl}`;
+  console.log("url", url);
+  console.log("request", req);
+  request.get(url, (err, response, body) => {
+    if (response.statusCode === 400) {
+      res.send([]);
+    } else {
+      res.send(body);
+      console.log(body);
+    }
+  });
+});
+
+app.listen(PORT);
+
+console.log(`Ruby Gems proxy API is now running at: http://localhost:${PORT}`);
--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -3,13 +3,15 @@ import "firebase/firestore";
 import "firebase/auth";
 // Initialize Firebase
 
+console.log(process.env.NODE_ENV);
+
 var config = {
-  apiKey: "AIzaSyCbicDKt1r_pkdWOS756fwM6UwBXNNDjsk",
+  apiKey: process.env.REACT_APP_GOOGLE_API,
   authDomain: "boilerplate-845c7.firebaseapp.com",
   databaseURL: "https://boilerplate-845c7.firebaseio.com",
   projectId: "boilerplate-845c7",
   storageBucket: "boilerplate-845c7.appspot.com",
-  messagingSenderId: "1036506062646"
+  messagingSenderId: process.env.REACT_APP_MESSENGER_API
 };
 
 firebase.initializeApp(config);
